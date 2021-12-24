acffcd3e080002e4b7dc9a3b84aa48ec64b9ddd8
bendance <bendance@iu.edu>
2021-12-23 22:11:13 -0500

    added home section and about me

---
+++ my-app/src/AboutMe.js
@@ -0,0 +1,17 @@
+const AboutMe = (props) =>
+{
+    return(
+        <div id='about'>
+            <h2>About Me</h2>
+            <p>My name is Benjamin Dance and I am an entry level full-stack developer and a native of Indianapolis, Indiana.
+            I am about to graduate from Indiana University Bloomington in Spring of 2022 with a double major in Computer Science
+            and Interactive and Digital Media. Throughout my time in university, I've been an active member of the Code@IU club and
+            have even won their 2021 Hackathon. This is not as impressive as it sounds because I was the only person who participated.</p>
+            <p>In my freetime, I like helping my friends with various projects they're working on and I enjoy working on my own projects.
+            The project that I'm most proud of is a Spotify Playlist generator which scrapes the top 100 most popular songs in a genre and
+            compiles them into a Spotify playlist.</p>
+        </div>
+    )
+}
+
+export default AboutMe;
